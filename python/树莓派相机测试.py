84b461649612e3d8b8c02feb4af0f35a0a8cb22b
mxyr <yongren@adidesigns.asia>
2023-07-05 10:25:07 +0800

    相机测试

---
+++ python/树莓派相机测试.py
@@ -0,0 +1,16 @@
+from picamera2 import Picamera2
+
+picam_1 = Picamera2(0)
+picam_2 = Picamera2(1)
+config_1 = picam_1.create_still_configuration()
+config_2 = picam_1.create_still_configuration()
+picam_1.configure(config_1)
+picam_2.configure(config_2)
+print("初始化摄像头！")
+picam_1.start()
+picam_2.start()
+picam_1.capture_file("cam0.png")
+picam_2.capture_file("cam1.png")
+print("拍照成功！")
+picam_1.stop()
+picam_2.stop()
